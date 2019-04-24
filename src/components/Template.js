f46a2f4fafbb63522bebfdd5d919c6d388460d29
Davor <risteskidavor@gmail.com>
2019-04-24 15:41:50 -0400

    Stubs for Python, Server, Client SPA

---
+++ src/components/Template.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+/**
+ * Template file for components.
+ */
+export default class Template extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Placeholder</h1>
+            </div>
+        );
+    }
+
+}
