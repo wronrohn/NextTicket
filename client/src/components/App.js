d9ecafa096bb3505282a2666bf2d3d3ae4b51505
Sumit Oberoi <soberoi1@>
2019-04-30 21:09:30 -0400

    Dev Commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,30 @@
-
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navigation from "./Navigation";
+import LandingPage from "./Landing";
+import SignUpPage from "./Signup";
+import SignInPage from "./SignIn";
+import MoviesPage from "./Movies";
+import * as ROUTES from "../constants/routes";
 
 /**
  * Main entrypoint for the client-side applicaiton.
  */
 export default class App extends React.Component {
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src="./assets/logo.svg" className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a  className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Navigation />
+            <hr />
+            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+            <Route exact path={ROUTES.SIGNIN} component={SignInPage} />
+            <Route exact path={ROUTES.SIGNUP} component={SignUpPage} />
+            <Route exact path={ROUTES.MOVIES} component={MoviesPage} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
+
+const Navigation = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to={ROUTES.SIGNIN}>Sign In</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Navigation;
