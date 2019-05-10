17fe704a843ca103f635de54aeeca2534cb6ef0c
Rohnit Shetty <rshetty6@stevens.edu>
2019-05-09 23:57:08 -0400

    WebSpeechApi implemented

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,13 +1,60 @@
 import React from "react";
 import movies from "../data";
-import Movie from "./Movie";
+import PropTypes from 'prop-types'
+import SpeechRecognition from 'react-speech-recognition'
 
-const MovieList = () => (
-  <div className="container">
-    {movies.map(movie => (
-      <Movie key={movie.index} {...movie} />
-    ))}
+
+const options={
+  autoStart: false,
+  continuous: false
+}
+const propTypes = {
+  // Props injected by SpeechRecognition
+  transcript: PropTypes.string,
+  startListening: PropTypes.func,
+  resetTranscript: PropTypes.func,
+  browserSupportsSpeechRecognition: PropTypes.bool
+};
+
+const MovieList = ({
+  transcript,
+  startListening,
+  browserSupportsSpeechRecognition}) => {
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
+  return (
+    <div>
+      
+    <div>
+    <div>
+      
+      <link rel="stylesheet" href="style.css" />
+      <form>
+      <input type="text" placeholder="Search.." size="55" id="search"/> 
+      <button type="submit"><i className="fa fa-search" ></i></button>
+      <button  type="submit" id="mic" onClick={startListening}><i className="fa fa-microphone"></i></button>
+      </form>
+
+      <span>{transcript}</span>
+    </div>
+
+    
+
+    
+
+    
+    
   </div>
-);
+    </div>
+  );
+};
 
-export default MovieList;
+  
+    
+  
+
+MovieList.propTypes = propTypes;
+
+export default SpeechRecognition(options)(MovieList);
