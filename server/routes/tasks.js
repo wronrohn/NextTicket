c130ab5cd0cc16b6638ca4de426e39c85371cd45
athiban p <athibanp@athibans-MacBook-Pro.local>
2019-05-10 16:15:50 -0400

    removed unwanted codes

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -83,79 +83,79 @@ router.put("/watchlist/", async (req, res) => {
 
 
 
-router.post("/", async (req, res) => {
-  try {
-    reqObj = req.body;
+// router.post("/", async (req, res) => {
+//   try {
+//     reqObj = req.body;
 
-    //Condition to check if keys exist in the json ob
-    if(!("title" in reqObj)) {throw "title is not defined."}
-    if(!("description" in reqObj)) {throw "description is not defined."}
-    if(!("hoursEstimated" in reqObj)) {throw "hoursEstimated is not defined."}
-    if(!("completed" in reqObj)) {throw "completed is not defined."}
-
-
-    if(!("comments" in reqObj)) {
-      reqObj.comments = []
-    }
+//     //Condition to check if keys exist in the json ob
+//     if(!("title" in reqObj)) {throw "title is not defined."}
+//     if(!("description" in reqObj)) {throw "description is not defined."}
+//     if(!("hoursEstimated" in reqObj)) {throw "hoursEstimated is not defined."}
+//     if(!("completed" in reqObj)) {throw "completed is not defined."}
 
 
-    // Check every key are in proper type and not empty
-    Object.keys(reqObj).forEach(function(key) {
-      if(key == "title"){
-        if(typeof reqObj[key] !==  "string") {
-         throw "Improper title provided";
-        }
-      }
-      if(key == "description"){
-        if(typeof reqObj[key] !==  "string") {
-         throw "Improper description provided";
-        }
-      }
-      if(key == "hoursEstimated"){
-        if(typeof reqObj[key] !==  "number") {
-         throw "Improper hoursEstimated provided";
-        }
-      }
-      if(key == "completed"){
-        if(typeof reqObj[key] !==  "boolean") {
-         throw "Improper completed provided ";
-        }
-      }
-      if(key == "comments"){
-        if(typeof reqObj[key] !==  "object") {
-         throw "Improper comments provided";
-        } else {
-          for (let i=0; i<reqObj[key].length; i++) {
-            reqObj[key][i]['id'] = uuid.v4();
-            if(reqObj[key][i].name === "" || typeof reqObj[key][i].name !== "string" ) {
-              throw "Improper name provided in the comments";
-            }
-            if(reqObj[key][i].comment === "" || typeof reqObj[key][i].comment !== "string" ) {
-              throw "Improper comment is provided";
-            }
-            // reqObj[key][i]['id'] = uuid.v4();
-          }
-        }
-      }
-    });
-    reqObj['id'] = uuid.v4();
-    reqObj._id = uuid.v4();
-    let newTask = {
-      _id: uuid.v4(),
-      id: uuid.v4(),
-      title: reqObj.title,
-      description: reqObj.description,
-      hoursEstimated: reqObj.hoursEstimated,
-      completed: reqObj.completed,
-      comments: reqObj.comments
+//     if(!("comments" in reqObj)) {
+//       reqObj.comments = []
+//     }
 
-    }
-    resultJSON = await taskData.addTask(newTask);
-    res.json(resultJSON);
-  } catch (error) {
-    res.status(500).json({ error: "Oops! Exception caught.", message: error });
-  }
-});
+
+//     // Check every key are in proper type and not empty
+//     Object.keys(reqObj).forEach(function(key) {
+//       if(key == "title"){
+//         if(typeof reqObj[key] !==  "string") {
+//          throw "Improper title provided";
+//         }
+//       }
+//       if(key == "description"){
+//         if(typeof reqObj[key] !==  "string") {
+//          throw "Improper description provided";
+//         }
+//       }
+//       if(key == "hoursEstimated"){
+//         if(typeof reqObj[key] !==  "number") {
+//          throw "Improper hoursEstimated provided";
+//         }
+//       }
+//       if(key == "completed"){
+//         if(typeof reqObj[key] !==  "boolean") {
+//          throw "Improper completed provided ";
+//         }
+//       }
+//       if(key == "comments"){
+//         if(typeof reqObj[key] !==  "object") {
+//          throw "Improper comments provided";
+//         } else {
+//           for (let i=0; i<reqObj[key].length; i++) {
+//             reqObj[key][i]['id'] = uuid.v4();
+//             if(reqObj[key][i].name === "" || typeof reqObj[key][i].name !== "string" ) {
+//               throw "Improper name provided in the comments";
+//             }
+//             if(reqObj[key][i].comment === "" || typeof reqObj[key][i].comment !== "string" ) {
+//               throw "Improper comment is provided";
+//             }
+//             // reqObj[key][i]['id'] = uuid.v4();
+//           }
+//         }
+//       }
+//     });
+//     reqObj['id'] = uuid.v4();
+//     reqObj._id = uuid.v4();
+//     let newTask = {
+//       _id: uuid.v4(),
+//       id: uuid.v4(),
+//       title: reqObj.title,
+//       description: reqObj.description,
+//       hoursEstimated: reqObj.hoursEstimated,
+//       completed: reqObj.completed,
+//       comments: reqObj.comments
+
+//     }
+//     resultJSON = await taskData.addTask(newTask);
+//     res.json(resultJSON);
+//   } catch (error) {
+//     res.status(500).json({ error: "Oops! Exception caught.", message: error });
+//   }
+// });
 
 /**
  * Wire up recommendation route.
