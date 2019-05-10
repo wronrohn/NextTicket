4b603bf86fd383e0c2b1ca33f24a1d3e123f93f5
Sumit Oberoi <sumit.oberoi1@gmail.com>
2019-05-10 04:12:38 -0400

    CSS Revamp

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -4,6 +4,20 @@ import Movie from "./Movie";
 
 const MovieList = () => (
   <div className="container">
+    <form className="form-inline mt-5 row no-gutters">
+      <label className="sr-only" htmlFor="search">
+        Search
+      </label>
+      <input
+        type="text"
+        className="form-control mb-2 mr-sm-2"
+        id="search"
+        placeholder="Search All Movies"
+      />
+      <button type="submit" class="btn btn-primary mb-2">
+        Search
+      </button>
+    </form>
     {movies.map(movie => (
       <Movie key={movie.index} {...movie} />
     ))}
