5c447941f08d6173e659247de8ed9ad675bd8173
Sumit Oberoi <sumit.oberoi1@gmail.com>
2019-05-10 10:02:31 -0400

    Navigation refactor

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,23 +1,10 @@
 import React from "react";
 import movies from "../data";
 import Movie from "./Movie";
+import Search from "./Search";
 
 const MovieList = () => (
-  <div className="container">
-    <form className="form-inline mt-5 row no-gutters">
-      <label className="sr-only" htmlFor="search">
-        Search
-      </label>
-      <input
-        type="text"
-        className="form-control mb-2 mr-sm-2"
-        id="search"
-        placeholder="Search All Movies"
-      />
-      <button type="submit" class="btn btn-primary mb-2">
-        Search
-      </button>
-    </form>
+  <div>
     {movies.map(movie => (
       <Movie key={movie.index} {...movie} />
     ))}
--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -2,5 +2,6 @@ export const LANDING = "/";
 export const SIGNUP = "/signup";
 export const SIGNIN = "/signin";
 export const MOVIES = "/movies";
+export const HOME = "/home";
 export const CHANGE_PASSWORD = "/changePassword";
 export const PASSWORD_FORGET = "/forgotPassword";
