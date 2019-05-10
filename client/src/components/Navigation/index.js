b3967e804d8812eddd8ab9cf13337fcd2df88241
Sumit Oberoi <sumit.oberoi1@gmail.com>
2019-05-10 01:33:52 -0400

    styling added to unauthenticate state

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -48,21 +48,42 @@ const NavigationAuth = () => (
 );
 
 const NavigationNonAuth = () => (
-  <nav className="navbar navbar-dark bg-dark">
-    <ul>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.SIGNIN}>Login</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.SIGNUP}>Sign Up</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.PASSWORD_FORGET}>ForgotPassword</Link>
-      </li>
-    </ul>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <Link className="navbar-brand" to={ROUTES.LANDING}>
+      Next Ticket
+    </Link>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span>
+        <i className="fa fa-align-justify" />
+      </span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li>
+          <Link to={ROUTES.SIGNUP} className="nav-link">
+            Sign Up
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to={ROUTES.SIGNIN} className="nav-link">
+            Login
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to={ROUTES.PASSWORD_FORGET} className="nav-link">
+            ForgotPassword
+          </Link>
+        </li>
+      </ul>
+    </div>
   </nav>
 );
 
