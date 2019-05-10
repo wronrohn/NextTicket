ea1514fe389b4a3cf84dd82adb8888cfd7e8bdb6
Rohnit Shetty <rshetty6@stevens.edu>
2019-05-10 14:29:30 -0400

    search component modfied and added webspeech component

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,32 +1,9 @@
 import React from "react";
-import PropTypes from 'prop-types'
-import SpeechRecognition from 'react-speech-recognition'
+import SpeechRecognition from './WebSpeech'
 
 
-const options ={
-  
-  continuous: false
-}
-
-const propTypes = {
-  // Props injected by SpeechRecognition
-  transcript: PropTypes.string,
-  startListening: PropTypes.func,
-  resetTranscript: PropTypes.func,
-  browserSupportsSpeechRecognition: PropTypes.bool
-};
-
-const Search =  ({
-   transcript,
-   resetTranscript,
-   startListening,
-   browserSupportsSpeechRecognition
-   }) => {
-     if (!browserSupportsSpeechRecognition) {
-       return null;
-     }
-     return(
-      <form className="mt-5 row no-gutters">
+const Search = () => (
+  <form className="mt-5 row no-gutters">
     <div className="input-group">
       <label className="sr-only" htmlFor="search">
         Search
@@ -36,20 +13,13 @@ const Search =  ({
         className="form-control mb-2 mr-sm-2"
         id="search"
         placeholder="Search All Movies"
-        
       />
       <button type="submit" class="btn btn-primary mb-2">
         Search
       </button>
-      {
-        /* { value of the transcript is in the "{transcript}" } */ }
-      <button type="submit" id="mic" onClick={{startListening} }><i className="fa fa-microphone"></i></button>
-      
-      
+      <SpeechRecognition />
+
     </div>
   </form>
-     );
-}
-  
-Search.propTypes = propTypes;
-export default SpeechRecognition(options)(Search);
+);
+export default Search;
