7106c3d169877b24c3c702c97f78216d1471b707
Sumit Oberoi <sumit.oberoi1@gmail.com>
2019-05-09 22:48:59 -0400

    Movie js file changed

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -10,37 +10,41 @@ const Navigation = ({ authUser }) => (
 );
 
 const NavigationAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.MOVIES}>Movies</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.CHANGE_PASSWORD}>Update Password</Link>
-    </li>
-    <li>
-      <SignOutButton />
-    </li>
-  </ul>
+  <nav className="navbar sticky-top navbar-dark">
+    <ul>
+      <li>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.MOVIES}>Movies</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.CHANGE_PASSWORD}>Update Password</Link>
+      </li>
+      <li>
+        <SignOutButton />
+      </li>
+    </ul>
+  </nav>
 );
 
 const NavigationNonAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.SIGNIN}>Login</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.SIGNUP}>Sign Up</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.PASSWORD_FORGET}>ForgotPassword</Link>
-    </li>
-  </ul>
+  <nav className="navbar sticky-top navbar-dark">
+    <ul>
+      <li>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.SIGNIN}>Login</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.SIGNUP}>Sign Up</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.PASSWORD_FORGET}>ForgotPassword</Link>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Navigation;
