f59de44a885f54bbd382268479ef26e8b40d5dcd
Sumit Oberoi <sumit.oberoi1@gmail.com>
2019-05-11 15:30:44 -0400

    Latest commit

---
+++ client/src/components/RecommendWatchListMenu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RecommendWatchListMenu = () => (
+  <ul>
+    <li>
+      <button>Recommended Movies</button>
+    </li>
+    <li>
+      <button>Recommended Movies</button>
+    </li>
+  </ul>
+);
+
+export default RecommendWatchListMenu;
