b1ee09cf217d0094c973933108941553d4e8382b
Sumit Oberoi <sumit.oberoi1@gmail.com>
2019-05-12 12:59:02 -0400

    Description added

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -7,7 +7,6 @@ import { compose } from "recompose";
 
 const SignupPage = () => (
   <div className="container bg-light contentCenter w-50 rounded-lg">
-    
     <SignUpForm />
   </div>
 );
@@ -77,57 +76,83 @@ class SignUpFormBase extends React.Component {
         {message && <p>{message}</p>}
         <div className="col-md-8 offset-md-2 mt-5">
           <h1 className="m-4 text-center">Welcome to Next Ticket</h1>
-          <h3 className="m-5 text-center">Sign Up and start getting recommendations for movies based on the ones you have already watched</h3>
-           <form onSubmit={this.onSubmit}>
-          <label  className="text-white sr-only" htmlFor={CONSTANTS.FORMFIELDUSERNAME}>Username:</label>
-          <input
-            type="text"
-            name={CONSTANTS.FORMFIELDUSERNAME}
-            id={CONSTANTS.FORMFIELDUSERNAME}
-            placeholder="Enter UserName"
-            value={userName}
-            class="form-control form-control-lg mb-4"
-            onChange={this.onFormFieldChange}
-          />
-          <label className="text-white sr-only" htmlFor={CONSTANTS.FORMFIELDEMAIL}>Email</label>
-          <input
-            type="text"
-            name={CONSTANTS.FORMFIELDEMAIL}
-            id={CONSTANTS.FORMFIELDEMAIL}
-            placeholder="Enter Email"
-            value={email}
-            class="form-control form-control-lg mb-4"
-            onChange={this.onFormFieldChange}
-          />
-          <label className="text-white sr-only" htmlFor={CONSTANTS.FORMFIELDPASSWORD}>Password</label>
-          <input
-            type="password"
-            name={CONSTANTS.FORMFIELDPASSWORD}
-            placeholder="Enter your password"
-            id={CONSTANTS.FORMFIELDPASSWORD}
-            value={password}
-            class="form-control form-control-lg"
-            onChange={this.onFormFieldChange}
-          />
-          <br />
-          <label className="text-white sr-only" htmlFor={CONSTANTS.FORMFIELDREPEATPASSWORD}>Password</label>
-          <input
-            type="password"
-            name={CONSTANTS.FORMFIELDREPEATPASSWORD}
-            placeholder="Re-enter your password"
-            id={CONSTANTS.FORMFIELDREPEATPASSWORD}
-            value={repeatPassword}
-            class="form-control form-control-lg"
-            onChange={this.onFormFieldChange}
-          />
-          <br />
-          <button className="btn btn-primary mb-2 mt-3 form-control form-control-lg" disabled={isInvalid} type="submit">
-            Sign Up
-          </button>
-          {error && <p>{error.message}</p>}
-        </form>
+          <h3 className="m-5 text-center">
+            Sign Up and start getting recommendations for movies based on the
+            ones you have already have in your watch list
+          </h3>
+          <form onSubmit={this.onSubmit}>
+            <label
+              className="text-white sr-only"
+              htmlFor={CONSTANTS.FORMFIELDUSERNAME}
+            >
+              Username:
+            </label>
+            <input
+              type="text"
+              name={CONSTANTS.FORMFIELDUSERNAME}
+              id={CONSTANTS.FORMFIELDUSERNAME}
+              placeholder="Enter UserName"
+              value={userName}
+              class="form-control form-control-lg mb-4"
+              onChange={this.onFormFieldChange}
+            />
+            <label
+              className="text-white sr-only"
+              htmlFor={CONSTANTS.FORMFIELDEMAIL}
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              name={CONSTANTS.FORMFIELDEMAIL}
+              id={CONSTANTS.FORMFIELDEMAIL}
+              placeholder="Enter Email"
+              value={email}
+              class="form-control form-control-lg mb-4"
+              onChange={this.onFormFieldChange}
+            />
+            <label
+              className="text-white sr-only"
+              htmlFor={CONSTANTS.FORMFIELDPASSWORD}
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name={CONSTANTS.FORMFIELDPASSWORD}
+              placeholder="Enter your password"
+              id={CONSTANTS.FORMFIELDPASSWORD}
+              value={password}
+              class="form-control form-control-lg"
+              onChange={this.onFormFieldChange}
+            />
+            <br />
+            <label
+              className="text-white sr-only"
+              htmlFor={CONSTANTS.FORMFIELDREPEATPASSWORD}
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name={CONSTANTS.FORMFIELDREPEATPASSWORD}
+              placeholder="Re-enter your password"
+              id={CONSTANTS.FORMFIELDREPEATPASSWORD}
+              value={repeatPassword}
+              class="form-control form-control-lg"
+              onChange={this.onFormFieldChange}
+            />
+            <br />
+            <button
+              className="btn btn-primary mb-5 mt-3 form-control form-control-lg"
+              disabled={isInvalid}
+              type="submit"
+            >
+              Sign Up
+            </button>
+            {error && <p>{error.message}</p>}
+          </form>
         </div>
-       
       </React.Fragment>
     );
   }
