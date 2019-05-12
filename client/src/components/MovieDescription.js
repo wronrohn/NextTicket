c569df27b1ee5f21382f5ae31e41a50b725ad0bf
athiban p <athibanp@athibans-MacBook-Pro.local>
2019-05-12 15:56:32 -0400

    css - done for movie-description page

--- client/src/components/MovieDescription.js
+++ client/src/components/MovieDescription.js
@@ -37,14 +37,22 @@ class MovieDescription extends Component {
           </div>
           <div className="row">
             <div className="offset-2 col-8 mt-3 text-white">
-              <h1 className="display-4">{title}</h1>
-              <p>
-                Genre: {genre} {subgenre && ` - ${subgenre}`}
-              </p>
-              {theme && <p>Theme: {theme}</p>}
-              <button className="btn btn-primary wishlist-cta-btn">
-                Add to Watch list
-              </button>
+              <div>
+                <h1 className="display-4">{title}</h1>
+              </div>
+              <div className="row">
+                <div className="col-md-8 col-lg-8 col-sm-8 col-xs-12">
+                  <p>
+                    Genre: {genre} {subgenre && ` - ${subgenre}`}
+                  </p>
+                  {theme && <p>Theme: {theme}</p>}
+                </div>
+                <div className="col-md-4 col-lg-4 col-sm-4 col-xs-12 content-align-left">
+                  <button className="btn btn-primary wishlist-cta-btn">
+                    Add to Watch list
+                  </button>
+                </div>
+              </div>
               {description && <h2>Description</h2>}
               <p className="lead">{description}</p>
             </div>
