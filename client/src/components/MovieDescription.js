161d61a452f0ec066e8e4464b11e0540a9fd824c
Sumit Oberoi <sumit.oberoi1@gmail.com>
2019-05-13 05:41:24 -0400

    Title changes

--- client/src/components/MovieDescription.js
+++ client/src/components/MovieDescription.js
@@ -3,7 +3,7 @@ import Network from "./Network";
 import { withAuthorization } from "../Session";
 import { withFirebase } from "../Firebase";
 import Error from "./Error";
-
+import { AuthUserContext } from "../Session";
 class MovieDescription extends Component {
   constructor(props) {
     super(props);
@@ -13,6 +13,8 @@ class MovieDescription extends Component {
     };
     super(props);
     this.network = new Network();
+    this.watchlistButtonClicked = this.watchlistButtonClicked.bind(this);
+    this.removeFromWatchList = this.removeFromWatchList.bind(this);
   }
   async componentDidMount() {
     const { location } = this.props;
@@ -32,6 +34,19 @@ class MovieDescription extends Component {
       });
     }
   }
+
+  async watchlistButtonClicked(userID) {
+    const { _id: movieID } = this.state.movie;
+    const movie = await this.network.addMovieToWatchList(userID, movieID);
+    this.setState({ movie: movie });
+  }
+
+  async removeFromWatchList(userID) {
+    const { _id: movieID } = this.state.movie;
+    const movie = await this.network.removeMovieFromWatchlist(userID, movieID);
+    this.setState({ movie: movie });
+  }
+
   render() {
     const { movie, error } = this.state;
     if (error) {
@@ -42,7 +57,15 @@ class MovieDescription extends Component {
       }
     }
     if (movie) {
-      const { title, description, genre, subgenre, theme, movie: name } = movie;
+      const {
+        title,
+        description,
+        genre,
+        subgenre,
+        theme,
+        movie: name,
+        watchlist
+      } = movie;
       return (
         <div className="container">
           <div className="row movie-description-inner">
@@ -65,11 +88,43 @@ class MovieDescription extends Component {
                       Genre: {genre} {subgenre && ` - ${subgenre}`}
                     </p>
                     {theme && <p>Theme: {theme}</p>}
+                    {watchlist && watchlist.length > 0 && (
+                      <p className="watch-list-text">
+                        {`${watchlist.length} ${
+                          watchlist.length > 1 ? `persons have` : `person has`
+                        } this movie in their watchlist`}
+                      </p>
+                    )}
                   </div>
+
                   <div className="col-md-4 col-lg-4 col-sm-4 col-xs-12">
-                    <button className="btn btn-primary wishlist-cta-btn">
-                      Add to Watch list
-                    </button>
+                    <AuthUserContext.Consumer>
+                      {authUser => {
+                        if (watchlist && watchlist.includes(authUser.uid)) {
+                          return (
+                            <button
+                              className="btn btn-primary wishlist-cta-btn"
+                              onClick={e => {
+                                this.removeFromWatchList(authUser.uid);
+                              }}
+                            >
+                              Remove from Watchlist
+                            </button>
+                          );
+                        } else {
+                          return (
+                            <button
+                              className="btn btn-primary wishlist-cta-btn"
+                              onClick={e => {
+                                this.watchlistButtonClicked(authUser.uid);
+                              }}
+                            >
+                              Add to Watch list
+                            </button>
+                          );
+                        }
+                      }}
+                    </AuthUserContext.Consumer>
                   </div>
                 </div>
                 {description && <h2>Description</h2>}
