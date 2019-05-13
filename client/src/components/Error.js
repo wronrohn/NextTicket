ab0780c6ae8a30da83a76b134bcd7e5daec08a6b
athiban p <athibanp@athibans-MacBook-Pro.local>
2019-05-13 13:09:59 -0400

    css for error container - updated

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -3,8 +3,10 @@ import React from "react";
 const Error = ({ message }) => (
   <div className="">
     <div className="center-content">
-      <h1 className="display-4 text-white">Error</h1>
-      <p className="lead m-5m text-white">{message}</p>
+      <div className="white-container">
+        <h1 className="display-4 ">Error</h1>
+        <p className="lead m-5m ">{message}</p>
+      </div>
     </div>
   </div>
 );
