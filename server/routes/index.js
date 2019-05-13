8e6492ad11b03da8621d748752ff34a68caa0f0c
Sumit Oberoi <sumit.oberoi1@gmail.com>
2019-05-13 14:31:33 -0400

    Renamed files

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,24 +1,23 @@
-
 const express = require("express");
-const movieRoutes = require("./tasks");
+const movieRoutes = require("./movies");
 const searchRoutes = require("./search");
 const imageRoutes = require("./image");
 
 const constructorMethod = app => {
-    app.use("/api/movies", movieRoutes);
-    app.use("/api/search", searchRoutes);
+  app.use("/api/movies", movieRoutes);
+  app.use("/api/search", searchRoutes);
 
-    // NOTICE: imageRoutes (image module) requires an express.static
-    //         middleware to work as intended. The sequence below is
-    //         expected to be present so that images are served correctly.
-    const static = express.static('static/images/');
-    app.use("/images", static);
-    app.use("/images/:genre", static);
-    app.use("/images", imageRoutes);
+  // NOTICE: imageRoutes (image module) requires an express.static
+  //         middleware to work as intended. The sequence below is
+  //         expected to be present so that images are served correctly.
+  const static = express.static("static/images/");
+  app.use("/images", static);
+  app.use("/images/:genre", static);
+  app.use("/images", imageRoutes);
 
-    app.use("*", (req, res) => {
-        res.status(404).json({ error: "Not found" });
-    });
+  app.use("*", (req, res) => {
+    res.status(404).json({ error: "Not found" });
+  });
 };
 
 module.exports = constructorMethod;
